8b0eb1e3502478bec750c7b1b9545652aed1e21d
LamIrvin <irvin.chris.lam@gmail.com>
2020-01-30 21:19:03 -0500

    implemented Chase Behaviour

--- Assets/GaboQuest/Scripts/AI/FSM/SharedStates/BM_Chase.cs
+++ Assets/GaboQuest/Scripts/AI/FSM/SharedStates/BM_Chase.cs
@@ -5,20 +5,88 @@ using BehaviourMachine;
 
 public class BM_Chase : StateBehaviour
 {
+    Agent m_agent;
+
+    GameObjectVar target;
+    Vector3Var destination;
+
+    [SerializeField]
+    private float ticks = .2f;
+
+    float sqrDistanceToTarget;
+
+    public float distanceToReachTarget = 1.5f;
+    float sqrDistanceToReachTarget;
+
 	// Called when the state is enabled
 	void OnEnable () {
 		Debug.Log("Started Chase");
+
+        m_agent = GetComponent<Agent>();
+
+        Setup();
+
+        SetAgentSpeed();
+
+        StartCoroutine(StartChasing());
 	}
- 
-	// Called when the state is disabled
-	void OnDisable () {
+
+    // Called when the state is disabled
+    void OnDisable () {
 		Debug.Log("Stopped Chase");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        StopAllCoroutines();
+    }
+
+
+    void Setup()
+    {
+        m_agent = GetComponent<Agent>();
+        target = blackboard.GetGameObjectVar("Threat");
+        destination = blackboard.GetVector3Var("Destination");
+        sqrDistanceToReachTarget = distanceToReachTarget * distanceToReachTarget;
+
+    }
+    private void SetAgentSpeed()
+    {
+        m_agent.m_navAgent.speed = m_agent.agentProperties.RunSpeed;
+        m_agent.m_navAgent.angularSpeed = m_agent.agentProperties.RunAngularSpeed;
+        m_agent.m_navAgent.acceleration = m_agent.agentProperties.RunAcceleration;
+    }
+
+    void calculateDistanceFromTarget()
+    {
+        sqrDistanceToTarget = (target.transform.position - transform.position).sqrMagnitude;
+    }
+
+    IEnumerator StartChasing()
+    {
+        while (enabled)
+        {
+            if (target.Value != null)
+            {
+                calculateDistanceFromTarget();
+                destination.Value = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+                m_agent.m_navAgent.SetDestination(destination.Value);
+            }
+
+
+            if (sqrDistanceToTarget < sqrDistanceToReachTarget)
+            {
+                SendEvent("ReachedTarget");
+                yield return null;
+            }
+
+            if (target.Value == null)
+            {
+                SendEvent("LostEnemy");
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(ticks);
+        }
+    }
+
+
 }
 
 
