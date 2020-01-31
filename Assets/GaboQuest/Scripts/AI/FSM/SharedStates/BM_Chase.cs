317d262fc2ee6225a3ef4a03d3083cf33f49dbfe
LamIrvin <irvin.chris.lam@gmail.com>
2020-01-30 20:36:40 -0500

    implemented Idle Behaviour

---
+++ Assets/GaboQuest/Scripts/AI/FSM/SharedStates/BM_Chase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using BehaviourMachine;
+
+public class BM_Chase : StateBehaviour
+{
+	// Called when the state is enabled
+	void OnEnable () {
+		Debug.Log("Started Chase");
+	}
+ 
+	// Called when the state is disabled
+	void OnDisable () {
+		Debug.Log("Stopped Chase");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
---
+++ Assets/GaboQuest/Scripts/AI/FSM/SharedStates/BM_Rest.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using BehaviourMachine;
+
+public class BM_Rest : StateBehaviour
+{
+    Agent m_agent;
+
+    public float IdleMinTime;
+    public Vector2 IdleTimeAddRange;
+    public float idleTime;
+
+    // Called when the state is enabled
+    void OnEnable () {
+		Debug.Log("Started *State*");
+
+        m_agent = GetComponent<Agent>();
+
+        StartCoroutine(StartIdling());
+	}
+
+
+    // Called when the state is disabled
+    void OnDisable () {
+		Debug.Log("Stopped *State*");
+	}
+	
+    void DetermineIdleTime()
+    {
+        if (idleTime == 0)
+            idleTime = IdleMinTime + Random.Range(IdleTimeAddRange.x, IdleTimeAddRange.y);
+        else
+            return;
+    }
+
+    private IEnumerator StartIdling()
+    {
+        DetermineIdleTime();
+
+        m_agent.m_navAgent.isStopped = true;
+        yield return new WaitForSeconds(idleTime);
+        m_agent.m_navAgent.isStopped = false;
+
+        SendEvent("ResumePatrol");
+        SendEvent("ResumeWandering");
+    }
+}
+
+
