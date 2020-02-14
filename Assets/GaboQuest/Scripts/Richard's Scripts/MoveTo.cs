cf20255df6468dc9f4ccf1ab7b5bc866a02742d7
Richard <richrdhw@gmail.com>
2020-02-14 12:07:01 -0500

    Camera now follows on Y-axis

--- Assets/GaboQuest/Scripts/Richard's Scripts/MoveTo.cs
+++ Assets/GaboQuest/Scripts/Richard's Scripts/MoveTo.cs
@@ -6,14 +6,19 @@ public class MoveTo : MonoBehaviour
 {
     public Transform destination;
 
+    public Vector3 yDir;
     public Vector3 dir;
     public Vector3 distanceDir;
+    [Range(1, 50)]
+    public float ySpeed;
     [Range(1, 50)]
     public float firstSpeed;
     [Range(1, 50)]
     public float secondSpeed;
     [Range (1, 100)]
     public float chaseSpeed;
+    [Range(-5, 5)]
+    public float yPositionThreshold;
     [Range(1, 5)]
     public float firstPositionThreshold;
     [Range(1, 5)]
@@ -31,10 +36,19 @@ public class MoveTo : MonoBehaviour
 
     }
     private void Move(Transform destination)
-    { 
+    {
+        
         dir = Vector3.Normalize(destination.position - transform.position);
+
         Vector3 lerpDir = Vector3.Lerp(transform.position, dir, chaseSpeed);
         distanceDir = new Vector3(lerpDir.x, 0f, lerpDir.z);
+
+        yDir = new Vector3(0f, destination.position.y - transform.position.y, 0f);
+
+        if (yDir.y !=0)
+        {
+            transform.position += yDir * Time.smoothDeltaTime * ySpeed;
+        }
 
         if (Vector3.Distance(transform.position, destination.position) >= firstPositionThreshold)
         {
@@ -44,7 +58,6 @@ public class MoveTo : MonoBehaviour
                 if (Vector3.Distance(transform.position, destination.position) >= firstPositionThreshold)
                 {
                     transform.position += distanceDir * Time.smoothDeltaTime * secondSpeed;
-
                 }
             }
 
@@ -52,12 +65,14 @@ public class MoveTo : MonoBehaviour
             {
                 if (flipLookDir)
                 {
-                    transform.rotation = Quaternion.LookRotation(-lerpDir);
+                    transform.rotation = Quaternion.LookRotation(-distanceDir);
                 }
-                else { transform.rotation = Quaternion.LookRotation(lerpDir); }
+                else { transform.rotation = Quaternion.LookRotation(distanceDir); }
 
             }
         }
+
+        
     }
 
 }
