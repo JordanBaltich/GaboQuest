315bcd9d77179901253996d183e88eaea039628f
LamIrvin <irvin.chris.lam@gmail.com>
2020-02-13 19:44:17 -0500

    Added Additional Rock Assets

---
+++ Assets/GaboQuest/Scripts/AI/PatrolPoints.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PatrolPoints : Agent
+{
+    public List<GameObject> waypoints;
+
+    public int waypointIndex = 0;
+
+    public void WalkToWaypoint()
+    {
+        //print("Set destination: " + waypoints[waypointIndex].transform.position);
+        m_navAgent.SetDestination(waypoints[waypointIndex].transform.position);
+    }
+
+}
