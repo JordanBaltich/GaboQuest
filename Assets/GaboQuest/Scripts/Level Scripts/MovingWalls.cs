a93754f20c7466b2fa089e8c8acdecc1234fd64b
Mtjjrstccw1 <48991402+Mtjjrstccw1@users.noreply.github.com>
2020-02-15 18:50:49 -0500

    Incorporated Moving Wall with Button
    Pressing a button will raise or lower a wall now. Also incorporated folder for Level scripts specifically.

---
+++ Assets/GaboQuest/Scripts/Level Scripts/MovingWalls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWalls : MonoBehaviour
+{
+
+    public GameObject startPos;
+    public GameObject endPos;
+
+    public void moveUp()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, startPos.transform.position, 5 * Time.deltaTime);
+    }
+
+    public void moveDown()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, endPos.transform.position, 5 * Time.deltaTime);
+    }
+}
