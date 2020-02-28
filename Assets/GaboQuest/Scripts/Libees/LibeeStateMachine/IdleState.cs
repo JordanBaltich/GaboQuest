e73ff663d6062a3f590e2f1a75ad5b0cc6dc5535
Richard <richrdhw@gmail.com>
2020-02-28 07:28:15 -0500

    Made a simple random animation stsemachine for libees

---
+++ Assets/GaboQuest/Scripts/Libees/LibeeStateMachine/IdleState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class IdleState : StateMachineBehaviour
+{
+    Rigidbody m_body;
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        m_body = animator.gameObject.GetComponentInParent<Rigidbody>();
+        animator.speed *= Random.Range(0.5f, 1f);
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetFloat("RigidbodyVelocity", m_body.velocity.magnitude);
+        
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
