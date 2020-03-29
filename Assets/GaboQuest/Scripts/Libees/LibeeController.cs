6ef13e9f52987f8eb4966aaaeb75ee98d6081cd8
Richard <richrdhw@gmail.com>
2020-03-28 20:40:48 -0400

    123

--- Assets/GaboQuest/Scripts/Libees/LibeeController.cs
+++ Assets/GaboQuest/Scripts/Libees/LibeeController.cs
@@ -1,34 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Analytics;
 
 public class LibeeController : MonoBehaviour
 {
+    Animator m_StateMachine;
     BounceArc m_BounceArc;
     Rigidbody m_Body;
 
-    public int libeeHitting;
+    public Vector3 bounceTarget;
+
     public int EnemyLayerID, GroundLayerID, PlayerLayerID;
 
+    public bool bouncing;
+
     [SerializeField] float hitForce;
+
+    public float shotDistance;
+
+    public Vector3 shotDirection;
 
     private void Awake()
     {
         m_Body = GetComponent<Rigidbody>();
-
+        m_StateMachine = GetComponent<Animator>();
         m_BounceArc = GetComponent<BounceArc>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-       
-
         if (collision.gameObject.layer == GroundLayerID || collision.gameObject.layer == PlayerLayerID)
         {
             m_Body.isKinematic = false;
-            m_BounceArc.currentPosition = 0;
-            Destroy(m_BounceArc.currentLandingTarget);
+            ResetTriggers();
+
+            if (collision.gameObject.layer == PlayerLayerID)
+            {
+                m_StateMachine.SetTrigger("isPickedUp");
+            }
+            if (collision.gameObject.layer == GroundLayerID)
+            {
+                m_StateMachine.SetTrigger("isIdle");
+            }
         }
 
     }
@@ -37,51 +50,31 @@ public class LibeeController : MonoBehaviour
     {
         if (other.gameObject.layer == EnemyLayerID)
         {
-            Rigidbody enemyRB = other.gameObject.GetComponentInParent<Rigidbody>();
-            if (other.gameObject.tag == "Weakpoint")
+            if (!bouncing)
             {
-                libeeHitting += 1;
-                m_BounceArc.BounceOffTarget(other.transform);
-                other.gameObject.GetComponentInParent<Health>().TakeDamage(2);
-                Knockback(enemyRB);
-                Analytics.CustomEvent("LibeeHitEnemyWeakpoint", new Dictionary<string, object>
-            {
-                { "EnemyName", other.gameObject.name }
-            });
+                Rigidbody enemyRB = other.gameObject.GetComponentInParent<Rigidbody>();
+                if (other.gameObject.tag == "Weakpoint")
+                {
+                    other.gameObject.GetComponentInParent<Health>().TakeDamage(2);
+                }
+                else if (other.gameObject.tag == "HurtBox")
+                {
+                    other.gameObject.GetComponentInParent<Health>().TakeDamage(1);
+                }
 
-                Analytics.CustomEvent("LibeeHitsWeakpoint", new Dictionary<string, object>
-            {
-                { "NumberOfHits", libeeHitting }
-            });
-                
-            }
-            else if (other.gameObject.tag == "HitBox")
-            {
-                m_BounceArc.BounceOffTarget(other.transform);
-                other.gameObject.GetComponentInParent<Health>().TakeDamage(1);
+                bounceTarget = other.ClosestPointOnBounds(transform.position);
+                ResetTriggers();
+                m_StateMachine.SetTrigger("isBouncing");
+
                 Knockback(enemyRB);
 
-                Analytics.CustomEvent("LibeeHitEnemyButNotWeakpoint", new Dictionary<string, object>
-            {
-                { "EnemyName", other.gameObject.name }
-            });
-
-                Analytics.CustomEvent("LibeeHitsNotWeakpoint", new Dictionary<string, object>
-            {
-                { "NumberOfHits", libeeHitting }
-            });
+                if (other.gameObject.GetComponentInParent<Health>().currentHealth <= 0)
+                {
+                    other.gameObject.GetComponentInParent<Health>().DestroyObject();
+                }
 
             }
 
-            if (other.gameObject.GetComponentInParent<Health>().currentHealth <= 0)
-            {
-                other.gameObject.GetComponentInParent<Health>().DestroyObject();
-
-                Analytics.CustomEvent("LibeeKilledEnemy", new Dictionary<string, object>
-            {
-                { "EnemyName", other.gameObject.name }
-            });
-            }
         }
     }
 
@@ -91,5 +84,31 @@ public class LibeeController : MonoBehaviour
         Vector3 direction = givenBody.transform.position - transform.position;
 
         givenBody.AddForce(direction.normalized * hitForce, ForceMode.Impulse);
+    }
+
+    public void ResetTriggers()
+    {
+        m_StateMachine.ResetTrigger("isDead");
+        m_StateMachine.ResetTrigger("isIdle");
+        m_StateMachine.ResetTrigger("isBouncing");
+        m_StateMachine.ResetTrigger("isPickedUp");
+        m_StateMachine.ResetTrigger("isShot");
+    }
+
+    public IEnumerator BulletTravel(Vector3 end, float shotForce, AnimationCurve fallCurve, Transform AimPoint)
+    {
+        float time = 0;
+        Vector3 start = transform.position;
+
+        while (time <= shotForce)
+        {
+            float percent = time / shotForce;
+            time += Time.deltaTime;
+            end.y = Mathf.Lerp(start.y, AimPoint.position.y, fallCurve.Evaluate(percent));
+            m_Body.position = Vector3.Lerp(start, end, percent);
+
+
+            yield return null;
+        }
     }
 }
